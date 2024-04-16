f1b09e53c3fbca0dd6e417598e5dc5273bac623c
Nayem55 <nayem.sayed.web@gmail.com>
2024-04-16 14:23:17 +0600

    ..

--- pages/BoysFashion.js
+++ pages/BoysFashion.js
@@ -20,3 +20,41 @@ const BoysFashion = () => {
 }
 
 export default BoysFashion;
+
+
+// import Product from "@/Components/Product";
+// import useProduct from "../Hooks/useProduct";
+// import { useState } from "react";
+
+// const BoysFashion = ({ boysFashionProducts }) => {
+//   return (
+//     <div>
+//       <h1 className="text-primary text-center text-4xl my-10 font-bold">
+//         Boy's Fashion
+//       </h1>
+//       <div className="bg-secondary flex flex-wrap justify-center p-[20px]">
+//         {boysFashionProducts?.map((product) => (
+//           <Product product={product}></Product>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export async function getServerSideProps() {
+//   // Fetch data using your useProduct hook or any other method
+//   const res = await fetch("http://localhost:3000/api/products");
+//   const products = await res.json();
+
+//   // Filter products for boys fashion
+//   const boysFashionProducts = products.filter(
+//     (product) => product.category === "Boy's Fashion"
+//   );
+
+//   return {
+//     props: {
+//       boysFashionProducts,
+//     },
+//   };
+// }
+// export default BoysFashion;
